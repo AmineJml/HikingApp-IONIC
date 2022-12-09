73a6c96db3d43cfa6c7fbdf175e44cb363132bd6
AmineJml <aminejamal003@gmail.com>
2022-12-09 10:27:00 +0200

    adding get_comments api

---
+++ BackEnd/get_comments.php
@@ -0,0 +1,28 @@
+<?php 
+include("connection.php");
+
+if(isset($_GET["post_id"]) ){
+    $post_id = $_GET["post_id"];
+}else{
+    $response = [];
+    $response["success"] = false;   
+    echo json_encode($response);
+    return;  
+}
+
+
+$query = $mysqli->prepare("SELECT comment FROM comments WHERE post_id = ?");
+$query->bind_param("i", $post_id);
+$query->execute();
+
+$array = $query->get_result();
+$response = [];
+while($comments = $array->fetch_assoc()){
+    $response[] = $comments;
+}
+
+if(!$response ){ //list is empty
+    $response["success"] = "no_comments_for_this_post";   
+}
+
+echo json_encode($response);
