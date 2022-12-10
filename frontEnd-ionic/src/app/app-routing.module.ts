a9ed3fbc166ce02571dcba33e41d2598d43fc6a8
AmineJml <aminejamal003@gmail.com>
2022-12-10 21:35:00 +0200

    adding addPost page

--- frontEnd-ionic/src/app/app-routing.module.ts
+++ frontEnd-ionic/src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'add-post',
+    loadChildren: () => import('./add-post/add-post.module').then( m => m.AddPostPageModule)
   }
 ];
 @NgModule({
